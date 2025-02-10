232602f75fbe0b996d11c01444efc61ca3aa4bfd
Madhav <vmadhav1805@gmail.com>
2025-02-10 17:09:28 +0530

    map content

--- src/pages/Maps.js
+++ src/pages/Maps.js
@@ -1,87 +1,153 @@
-import React from "react";
-import { MapContainer, TileLayer, Marker, Popup, Polyline, Polygon, Rectangle } from "react-leaflet";
+import React, { useState } from "react";
+import L from "leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  Marker,
+  Popup,
+  Polyline,
+  Polygon,
+  Rectangle,
+} from "react-leaflet";
 import "leaflet/dist/leaflet.css";
-import Icon from "../assets/images/bettercallsaul.jpg";
+import Pin from "../assets/images/marker.png";
+import Document from "../assets/images/document.pdf";
+import data from "../data/demo_all_sites_data.json";
 
 const Maps = () => {
+  const position = [17.18578, 78.3344];
 
-    const position = [51.505, -0.09];
+  const customIcon = L.icon({
+    iconUrl: Pin,
+    iconSize: [32, 32], // Adjust size if needed
+    iconAnchor: [16, 32], // Positioning relative to point
+    popupAnchor: [0, -32], // Positioning for popup
+  });
 
-    const polyline = [
-        [51.505, -0.09],
-        [51.51, -0.1],
-        [51.51, -0.12],
-    ]
+  const polyline = [];
 
-    const multiPolyline = [
-        [
-            [51.5, -0.1],
-            [51.5, -0.12],
-            [51.52, -0.12],
-        ],
-        [
-            [51.5, -0.05],
-            [51.5, -0.06],
-            [51.52, -0.06],
-        ],
-    ]
+  const multiPolyline = [
+    [
+      [51.5, -0.1],
+      [51.5, -0.12],
+      [51.52, -0.12],
+    ],
+    [
+      [51.5, -0.05],
+      [51.5, -0.06],
+      [51.52, -0.06],
+    ],
+  ];
 
-    const polygon = [
-        [51.515, -0.09],
-        [51.52, -0.1],
-        [51.52, -0.12],
-    ]
+  const polygon = [
+    [17.18425, 78.32895],
+    [17.18719, 78.32859],
+    [17.1902, 78.33517],
+    [17.19143, 78.3404],
+    [17.18554, 78.34323],
+  ];
 
-    const multiPolygon = [
-        [
-            [51.51, -0.12],
-            [51.51, -0.13],
-            [51.53, -0.13],
-        ],
-        [
-            [51.51, -0.05],
-            [51.51, -0.07],
-            [51.53, -0.07],
-        ],
-    ]
+  const multiPolygon = [
+    [
+      [51.51, -0.12],
+      [51.51, -0.13],
+      [51.53, -0.13],
+    ],
+    [
+      [51.51, -0.05],
+      [51.51, -0.07],
+      [51.53, -0.07],
+    ],
+  ];
 
-    const rectangle = [
-        [51.49, -0.08],
-        [51.5, -0.06],
-    ]
+  const rectangle = [
+    [51.49, -0.08],
+    [51.5, -0.06],
+  ];
 
-    const fillBlueOptions = { fillColor: 'blue' }
-    const blackOptions = { color: 'black' }
-    const limeOptions = { color: 'lime' }
-    const purpleOptions = { color: 'purple' }
-    const redOptions = { color: 'red' }
+  const fillBlueOptions = { fillColor: "blue" };
+  const blackOptions = { color: "black" };
+  const limeOptions = { color: "lime" };
+  const purpleOptions = {
+    Agricultural: { color: "green" },
+    "Non-Agricultural": { color: "yellow" },
+  };
+  const redOptions = { color: "red" };
 
-    return (
-        <div style={{ height: "100vh", width: "100%" }}>
-            <MapContainer
-                center={position}
-                zoom={15}
-                scrollWheelZoom={false}
-                style={{ height: "100%", width: "100%" }}
-            >
-                <TileLayer
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                />
-                <Marker position={position}>
-                    <Popup>
-                        A pretty CSS3 popup. <br /> Easily customizable.
-                    </Popup>
+  return (
+    <div className="w-full h-screen p-4">
+      <div className="rounded-[8px] h-full w-full overflow-hidden">
+        <MapContainer
+          center={position}
+          zoom={8}
+          scrollWheelZoom={false}
+          style={{ height: "100%", width: "100%" }}
+        >
+          <TileLayer
+            url="https://{s}.google.com/vt/lyrs=s&x={x}&y={y}&z={z}"
+            maxZoom={20}
+            subdomains={["mt1", "mt2", "mt3"]}
+          />
+
+          {data.slice(0, 5).map((item, index) => {
+            const position = [
+              parseFloat(item.latitude_decimal),
+              parseFloat(item.longitude_decimal),
+            ];
+
+            console.log(position);
+            return (
+              <>
+                <Marker key={index} icon={customIcon} position={position}>
+                  <Popup>
+                    <b>
+                      {" "}
+                      {item?.site_id} + {item?.village}
+                    </b>{" "}
+                    <br /> <br /> 📅 Purchase Date: {item.conv_proceeding_date}{" "}
+                    <br />
+                    📏 Site Area: 5 Acres, 20 Guntas <br />
+                    🏢 Company : {item?.vendee_name}
+                    <br />
+                    📜 Sale Deed :{" "}
+                    <a href={Document} target="_blank">
+                      {item.document_number}.pdf
+                    </a>
+                  </Popup>
                 </Marker>
+                {item?.polygon && (
+                  <Polygon
+                    pathOptions={purpleOptions[item?.property_type]}
+                    positions={item?.polygon}
+                  />
+                )}
+              </>
+            );
+          })}
 
-                <Polyline pathOptions={limeOptions} positions={polyline} />
-                <Polyline pathOptions={limeOptions} positions={multiPolyline} />
-                <Polygon pathOptions={purpleOptions} positions={polygon} />
-                <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
-                <Rectangle bounds={rectangle} pathOptions={blackOptions} />
-            </MapContainer>
-        </div>
-    );
+          {/* <Marker icon={customIcon} position={position}>
+            <Popup>
+              <b> Site 1 - Sunandanam</b> <br /> <br /> 📅 Purchase Date:
+              15-01-2024 <br />
+              📏 Site Area: 5 Acres, 20 Guntas <br />
+              🏢 Company : My Home Ventures Pvt Ltd.
+              <br />
+              📜 Sale Deed :{" "}
+              <a href={Document} target="_blank">
+                TS123456.pdf
+              </a>
+            </Popup>
+          </Marker> */}
+
+          {/* <Polyline pathOptions={limeOptions} positions={polyline} />
+          <Polyline pathOptions={limeOptions} positions={multiPolyline} /> */}
+
+          {/* <Polygon pathOptions={purpleOptions} positions={multiPolygon} />
+          <Rectangle bounds={rectangle} pathOptions={blackOptions} /> */}
+        </MapContainer>
+      </div>
+    </div>
+  );
 };
 
 export default Maps;
